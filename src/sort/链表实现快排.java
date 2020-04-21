2196cc33a7ff10aec87a56db977f7f0176c72e81
hl <hl7226261@2980.com>
2020-04-21 23:40:49 +0800

    leetcode

---
+++ src/sort/链表实现快排.java
@@ -0,0 +1,39 @@
+package sort;
+
+import node.entity.ListNode;
+
+public class 链表实现快排 {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(4);
+        a.next = new ListNode(2);
+        a.next.next = new ListNode(1);
+        a.next.next.next = new ListNode(3);
+        System.out.println(a);
+        System.out.println(insertionSortList(a));
+    }
+
+    public static ListNode insertionSortList(ListNode head) {
+        ListNode result = new ListNode(Integer.MIN_VALUE);
+        while(head!=null){
+            ListNode curr = new ListNode(head.val) ;
+            if(result.next==null){
+                result.next = curr;
+            }else{
+                ListNode resultCurr = result;
+                while(result!=null){
+                    if(curr.val>resultCurr.val&&curr.val<resultCurr.next.val){
+                        curr.next = resultCurr.next;
+                        resultCurr.next = curr;
+                        break;
+                    }else{
+                        resultCurr = resultCurr.next;
+                    }
+                }
+            }
+
+            head = head.next;
+        }
+
+        return result.next;
+    }
+}
