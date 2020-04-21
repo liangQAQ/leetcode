cccf085acf9dc633ed5792fa2150eae7f5d70d25
hl <hl7226261@2980.com>
2020-04-22 00:01:20 +0800

    leetcode

--- src/sort/链表实现快排.java
+++ src/sort/链表实现快排.java
@@ -4,10 +4,11 @@ import node.entity.ListNode;
 
 public class 链表实现快排 {
     public static void main(String[] args) {
-        ListNode a = new ListNode(4);
-        a.next = new ListNode(2);
-        a.next.next = new ListNode(1);
-        a.next.next.next = new ListNode(3);
+        ListNode a = new ListNode(-1);
+        a.next = new ListNode(5);
+        a.next.next = new ListNode(3);
+        a.next.next.next = new ListNode(4);
+        a.next.next.next.next = new ListNode(0);
         System.out.println(a);
         System.out.println(insertionSortList(a));
     }
@@ -21,13 +22,15 @@ public class 链表实现快排 {
             }else{
                 ListNode resultCurr = result;
                 while(result!=null){
-                    if(curr.val>resultCurr.val&&curr.val<resultCurr.next.val){
+                    if(resultCurr.next==null){
+                        resultCurr.next = curr;
+                        break;
+                    }else if(curr.val>resultCurr.val&&curr.val<resultCurr.next.val){
                         curr.next = resultCurr.next;
                         resultCurr.next = curr;
                         break;
-                    }else{
-                        resultCurr = resultCurr.next;
                     }
+                    resultCurr = resultCurr.next;
                 }
             }
 
